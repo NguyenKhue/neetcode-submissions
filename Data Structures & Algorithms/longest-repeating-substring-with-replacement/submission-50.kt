a37972ba71d8c45e9caaa4dfd702c72fb8cc6908
NguyenKhue <121930221+NguyenKhue@users.noreply.github.com>
2026-07-18 22:28:04 +0700

    Add: longest-repeating-substring-with-replacement - submission-50

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-50.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        val counts = hashMapOf<Char, Int>()
+        var maxCount = 0
+        var l = 0
+        var maxRepeat = 0
+
+        for((r, c) in s.withIndex()) {
+            counts[c] = (counts[c] ?: 0) + 1
+            maxCount = maxOf(maxCount, counts[c]!!)
+
+            while(r - l + 1 - maxCount > k) {
+                l++
+                counts[s[l-1]] = counts[s[l-1]]!! - 1 
+            }
+
+            maxRepeat = r - l + 1
+        }
+
+        return maxRepeat;
+    }
+}
