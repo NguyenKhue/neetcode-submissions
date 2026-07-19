03bcc97736a9254b34d79318112229b7c367afc8
NguyenKhue <121930221+NguyenKhue@users.noreply.github.com>
2026-07-19 09:33:54 +0700

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.kt
@@ -0,0 +1,41 @@
+class Solution {
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        if(s1.length > s2.length) return false
+        
+        val s1Map = mutableMapOf<Char, Int>()
+
+        for(c in s1) {
+            s1Map[c] = s1Map.getOrDefault(c, 0) + 1
+        }
+
+        var l = 0
+        
+        for((r, c) in s2.withIndex()) {
+            if(s1Map.containsKey(c).not()) {
+                while(l <= r) {
+                    if(s1Map.containsKey(s2[l])) {
+                        s1Map[s2[l]] = s1Map[s2[l]]!! + 1
+                    }
+
+                    l++
+                }
+            } else  {
+                if(s1Map[c]!! == 0) {
+                    while(s1Map[c]!! == 0) {
+                        if(s1Map.containsKey(s2[l])) {
+                            s1Map[s2[l]] = s1Map[s2[l]]!! + 1
+                        }
+
+                        l++
+                    }
+                }
+
+                s1Map[c] = s1Map[c]!! - 1
+            }
+
+            if(r - l + 1 == s1.length) return true;
+        }
+
+        return false;
+    }
+}
